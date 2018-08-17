4523cd4a5ccebb7d3844842e3c29348723bd1dc0
Joseph Bauer <bauerj@DESKTOP-VF819NP.localdomain>
2018-08-17 14:46:47 -0400

    Removed the download folder and make a small script that will automate installing my vim plugins.

---
+++ vimInstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing pathogen.."
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+
+echo "Installing vim airline"
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+echo "Installing NerdTree"
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
