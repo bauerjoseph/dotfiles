6e3f1e00b16cefb0739f911fa80d1daa6a7f01fc
Joseph Bauer <bauerjoseph@protonmail.com>
2018-12-18 22:10:18 -0500

    Added bashrc and added git config commands

--- .minimalInstall.sh
+++ .minimalInstall.sh
@@ -2,17 +2,16 @@
 # https://www.lovelinux-skill.ooo/2018/07/the-ultimate-minimal-ubuntu.html
 sudo apt install i3 wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nautilus volumeicon-alsa
 
-sudo apt-get install git vim curl firefox feh pulseaudio sakura
+sudo apt-get install vim curl firefox feh pulseaudio sakura
+
+git config --global user.email "bauerjoseph@protonmail.com"
+git config --global user.name "Joseph Bauer"
 
 # Install vim package manager and a few packages I like
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 
-echo "Installing vim airline"
-
+# vim packages
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-
-echo "Installing NerdTree"
-
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
