2914718e859d67713b42bd986896b96bccb2d417
Joseph Bauer <bauerjoseph@protonmail.com>
2018-12-29 23:00:51 -0500

    Added stuff for building polybar. Not tested. Need to try out in a vm.

--- .minimalInstall.sh
+++ .minimalInstall.sh
@@ -17,3 +17,11 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# polybar
+# This needs testing
+sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev
+
+mkdir ~/.polybar
+git clone --branch 3.3 --recursive https://github.com/jaagr/polybar ~/.polybar
+./.polybar/build.sh
