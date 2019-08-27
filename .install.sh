cd02125478b085d5f903eb520d7498efcd8e0ac5
Joseph Bauer <root@thesudorm.com>
2019-08-26 22:01:55 -0400

    Working on install script for solus linux.

---
+++ .install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# List of packages to install for Solus
+Packages=(
+    xbacklight
+    neovim
+    neofetch
+    compton
+    feh
+    i3
+    rofi
+    polybar
+    git
+    cava
+)
+
+for pack in "${Packages[@]}"
+do
+    echo "Installing $pack"
+    sudo eopkg it $pack -y
+done
+
+echo 
+
+# Maybe I need to do this first.. Also should fail this if
+# there is no public ssh key yet.
+if [ ! -d "$HOME/.git" ]
+then 
+    echo "Getting dotfiles"
+    cd $HOME
+    git init 
+    git remote add origin git@github.com:thesudorm/dotfiles.git
+    git fetch --all 
+    git reset --hard origin/master
+fi
