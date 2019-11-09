73709171ff243e485c6291a0d8b431fe7d0d3714
Joseph Bauer <root@thesudorm.com>
2019-11-09 06:21:54 -0500

    Changed the two quit commands to a single quit all command. Also, need to add some functionality to test for curl and nvim being installed.

--- .vimInstall.sh
+++ .vimInstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Need to make sure that both neovim and curl
+# are both installed for this to work.
+
 echo "Making vim backup directory.."
 mkdir ~/.vimbackups
 
@@ -8,4 +11,4 @@ echo "Installing vim-plug.."
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim -c PlugInstall -c q! -c q!
+nvim -c PlugInstall -c qa!
