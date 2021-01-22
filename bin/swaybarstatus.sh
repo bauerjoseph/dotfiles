7a6bc97f9bb475f76dccf9d1b4776669d3e66d05
Joseph Bauer <root@thesudorm.com>
2021-01-21 21:30:46 -0500

    Learning to use awk in swaybarstatus script.

--- bin/swaybarstatus.sh
+++ bin/swaybarstatus.sh
@@ -5,13 +5,19 @@ IFS=' '
 while :
     DATE=$(date +'%b %d %l:%M %p')
     BATT=$(acpi -b)
+    VOL=$(amixer)
     PERC=''
+
     read -rasplitIFS<<< "$BATT"
     for word in "${splitIFS[@]}"; do
         if [[ $word == *"%"* ]]; then
             PERC=${word:0:-1}
         fi
     done
-    echo "${DATE} ${PERC}"
+
+    VOL=$(amixer | awk '/%/ {print $5}' | awk 'NR==1')
+    VOL=${VOL:1:-1}
+
+    echo "${DATE} BATT ${PERC} VOL ${VOL}"
     do sleep 1; 
 done
